e046cf84d4ce543eae4f650b389ee47ab1696618
Karthikkasiralla <karthikkasiralla16@gmail.com>
2025-03-07 19:11:09 +0530

    Updated messages

---
+++ restApi-demo/src/test/java/com/cloudvendor/restAPI/model/RestApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudvendor.restAPI.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
